52dc692b75a102755374ac4727d9cf6c96bcc36d
hemantkumar2 <hemantnirmalkar17@gmail.com>
2019-11-26 14:34:09 +0530

    handled submit and fetched data from inputs

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -9,17 +9,35 @@ export default class PostForm extends Component {
       body: ''
     }
     this.onChange = this.onChange.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
   }
   onChange(e) {
     this.setState({
       [e.target.name]: e.target.value
     })
   }
+  onSubmit(e) {
+    e.preventDefault()
+    const post = {
+      title: this.state.title,
+      body: this.state.body
+    }
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(post)
+    })
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch()
+  }
   render() {
     return (
       <div>
         <h1>Post</h1>
-        <form >
+        <form onSubmit={this.onSubmit}>
           <div>
             <label htmlFor="">Title:</label><br />
             <input type="text" name='title' onChange={this.onChange} value={this.state.title} />
@@ -28,7 +46,7 @@ export default class PostForm extends Component {
             <label htmlFor="">Title:</label><br />
             <textarea name='body' onChange={this.onChange} value={this.state.body} />
           </div><br />
-          <button type='submit'></button>
+          <button type='submit'>Submit</button>
         </form>
       </div>
     )
