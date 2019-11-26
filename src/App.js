7b26356d60d52fc07ac3321cd1b4fb31799b8c89
hemantkumar2 <hemantnirmalkar17@gmail.com>
2019-11-26 16:34:30 +0530

    installed redux react-redux redux-thunk, imported provider from react-redux,created actions store and reducer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react'
 import Posts from './components/Posts'
 import PostForm from './components/PostForm'
+import { Provider } from 'react-redux'
+import store from './components/store'
 import './App.css';
+
+
 export default class App extends Component {
   render() {
     return (
-      <div className='App'>
-        <PostForm />
-        <Posts />
-      </div>
+      <Provider store={store}>
+        <div className='App'>
+          <PostForm />
+          <hr />
+          <Posts />
+        </div>
+      </Provider>
     )
   }
 }
